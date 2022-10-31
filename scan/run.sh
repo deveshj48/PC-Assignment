b91a0541a4b583e0860571eee19159fbb9d14d06
Reece-172 <2325529@students.wits.ac.za>
2022-10-31 23:39:53 +0200

    Included the mpi implementation in the run.sh

--- scan/run.sh
+++ scan/run.sh
@@ -6,3 +6,7 @@ echo
 echo ---------scan_omp has started----------
 ./scan_omp 2 1 4 0 3 7 6 3
 echo ---------scan_omp is done-------------
+echo
+echo ---------scan_omp has started----------
+./scan_mpi 2 1 4 0 3 7 6 3
+echo ---------scan_omp is done-------------
